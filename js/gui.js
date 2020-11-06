455ea110fc95c1642ee3525688fad3be6e1a6aec
Marian Pekár <marian.pekar@gmail.com>
2020-11-06 19:19:44 +0100

    Fix toggle cell position bug and add World gui folder

--- js/gui.js
+++ js/gui.js
@@ -4,28 +4,29 @@ class Gui {
         this.world = game.world;
         this.game = game;
 
+        this.gui = new dat.GUI({ autoPlace: true });
+
         this.cursorPosition = {
             x: this.controller.cursor.position.x,
             y: this.controller.cursor.position.y,
             z: this.controller.cursor.position.z,
         }
-        this.toggler = false;
 
         this.setupCursorFolder();
+        this.setupWorldFolder();
     }
 
     setupCursorFolder() {
-        const gui = new dat.GUI({ autoPlace: true });
-        const cursorControlls = gui.addFolder("Cursor")
+        const cursorControlls = this.gui.addFolder("Cursor")
         cursorControlls.add(this.cursorPosition, 'x', 0, this.world.width - 1).name('X').step(1).onChange(this.updateCursorPosition.bind(this));
         cursorControlls.add(this.cursorPosition, 'y', 0, this.world.width - 1).name('Y').step(1).onChange(this.updateCursorPosition.bind(this));
         cursorControlls.add(this.cursorPosition, 'z', 0, this.world.width - 1).name('Z').step(1).onChange(this.updateCursorPosition.bind(this));
 
         this.toggleButton = { toggle: () => { 
-            game.toggleCellAtCoords(this.cursorPosition); 
+            this.game.toggleCellAtCoords(this.controller.cursor.position); 
         }};
-        gui.add(this.toggleButton, 'toggle').name("Toggle Cell");
 
+        cursorControlls.add(this.toggleButton, 'toggle').name("Toggle Cell");
         cursorControlls.open();
     }
 
@@ -36,5 +37,16 @@ class Gui {
 
     toggleCell() {
         this.game.toggleCellAtCoords(this.cursor.position); 
+    }
+
+    setupWorldFolder() {
+        const worldControlls = this.gui.addFolder("World");
+        
+        this.clearButton = { clear: () => { 
+            this.game.clear(); 
+        }};
+        worldControlls.add(this.clearButton, 'clear').name("Clear");
+
+        worldControlls.open();
     }
 }
