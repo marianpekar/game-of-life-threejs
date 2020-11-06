6449be3e4efd80d5c0298525f6a44a2f329d9df8
Marian Pekár <marian.pekar@gmail.com>
2020-11-06 19:49:59 +0100

    Refactor gui.setupRandomizeNBRsFolder out of gui.setupCursorFolder

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const settings = new Settings();
 const game = new Game(settings);
 
-game.setRandomNeighborsAlive(game.world.center);
+game.setRandomNeighborsAlive(game.world.center, 0.5);
 
 const gui = new Gui(game);
 new THREE.OrbitControls( game.sceneManager.camera, game.sceneManager.renderer.domElement );
